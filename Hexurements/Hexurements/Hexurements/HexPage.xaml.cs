1d64b7c1873e2d3822f40bbdf741d5bd52574d94
LuiD2 <47342745+LuiD2@users.noreply.github.com>
2021-02-12 19:34:02 -0700

    You can now take pictures

--- Hexurements/Hexurements/Hexurements/HexPage.xaml.cs
+++ Hexurements/Hexurements/Hexurements/HexPage.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 
 namespace Hexurements
@@ -13,6 +12,20 @@ namespace Hexurements
         public HexPage()
         {
             InitializeComponent();
+        }
+
+        private async void CameraButton_Clicked(object sender, EventArgs e)
+        {
+            // Documentation for this: https://www.xamarinhelp.com/use-camera-take-photo-xamarin-forms/
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+
+            if (photo != null)
+            {
+                PhotoImage.Source = ImageSource.FromStream(() =>
+                {
+                    return photo.GetStream();
+                });
+            }
         }
     }
 }
