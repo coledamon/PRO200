075e0268ef87b164e0ac02d6410a350da9034eac
WinnieQueen <ashilliard@student.neumont.edu>
2021-02-18 17:08:21 -0700

    Dark/Light Theme Added

--- Hexurements/Hexurements/Hexurements/MainPage.xaml.cs
+++ Hexurements/Hexurements/Hexurements/MainPage.xaml.cs
@@ -10,9 +10,20 @@ namespace Hexurements
 {
     public partial class MainPage : ContentPage
     {
+        private bool ogColor = true;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Style_Clicked(object sender, EventArgs e)
+        {
+            App.Current.Resources["TextColor"] = ogColor ? App.Current.Resources["AltTextColor"] : App.Current.Resources["OGTextColor"];
+            App.Current.Resources["BgColor"] = ogColor ? App.Current.Resources["AltBgColor"] : App.Current.Resources["OGBgColor"];
+            App.Current.Resources["BtnColor"] = ogColor ? App.Current.Resources["AltBtnColor"] : App.Current.Resources["OGBtnColor"];
+            StyleBtn.ImageSource = ogColor ? "sun.png" : "moon.png";
+            ogColor = !ogColor;
         }
 
         private async void HexPicker_Clicked(object sender, EventArgs e)
